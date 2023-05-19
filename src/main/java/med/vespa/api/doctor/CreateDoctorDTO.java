bf260d4f5bdc70dd343f37f76a44995c197eafa1
FabyGiacomini <fabygiacomini@ymail.com>
2023-05-19 09:08:38 -0300

    Add phone field on doctor entity

--- src/main/java/med/vespa/api/doctor/CreateDoctorDTO.java
+++ src/main/java/med/vespa/api/doctor/CreateDoctorDTO.java
@@ -10,14 +10,21 @@ import med.vespa.api.address.AddressDTO;
 public record CreateDoctorDTO(
         @NotBlank
         String name,
+
         @NotBlank
         @Email
         String email,
+
+        @NotBlank
+        String phone,
+
         @NotBlank
         @Pattern(regexp = "\\d{4,6}")
         String crm,
+
         @NotNull
         Specialty specialty,
+
         @NotNull
         @Valid
         AddressDTO address
