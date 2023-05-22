da1e917ad54ba5c6f17e85e81f0cdac5c9f8c5c4
FabyGiacomini <fabygiacomini@ymail.com>
2023-05-22 08:53:38 -0300

    Change page and sort default properties on doctor's list

--- src/main/java/med/vespa/api/controller/DoctorController.java
+++ src/main/java/med/vespa/api/controller/DoctorController.java
@@ -8,6 +8,7 @@ import med.vespa.api.doctor.ListDoctorsDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,7 @@ public class DoctorController {
     }
 
     @GetMapping
-    public Page<ListDoctorsDTO> list(Pageable pageable) {
+    public Page<ListDoctorsDTO> list(@PageableDefault(size = 10, sort = {"name"}) Pageable pageable) {
         return repository.findAll(pageable).map(ListDoctorsDTO::new);
     }
 }
