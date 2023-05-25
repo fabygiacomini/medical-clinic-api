a363578f3562458b1dc31ab1f9fbb3b669be81f6
FabyGiacomini <fabygiacomini@ymail.com>
2023-05-25 09:42:38 -0300

    Install JWT dependency and return token on login

--- src/main/java/med/vespa/api/controller/AuthenticationController.java
+++ src/main/java/med/vespa/api/controller/AuthenticationController.java
@@ -1,6 +1,9 @@
 package med.vespa.api.controller;
 
 import med.vespa.api.domain.user.AuthenticationDTO;
+import med.vespa.api.domain.user.User;
+import med.vespa.api.infra.security.TokenDTO;
+import med.vespa.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,11 +20,15 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity login(@RequestBody AuthenticationDTO data) {
-        var token = new UsernamePasswordAuthenticationToken(data.login(), data.password());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(data.login(), data.password());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.generateToken((User) authentication.getPrincipal());
+        return ResponseEntity.ok(new TokenDTO(tokenJWT));
     }
 }
---
+++ src/main/java/med/vespa/api/infra/security/TokenService.java
@@ -0,0 +1,36 @@
+package med.vespa.api.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.vespa.api.domain.user.User;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("API Vespa.med")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(expireDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Generate token error", exception);
+        }
+    }
+
+    private Instant expireDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
